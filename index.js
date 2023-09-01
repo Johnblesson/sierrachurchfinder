1a9691df25a4d313a5461425fbffb9bd59edc3c7
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-09-01 11:26:58 +0000

    update

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const hbs = require('hbs')
-const bcrypt = require('bcrypt')
-const mongoose = require('mongoose')
-require("dotenv").config()
-const Credential = require('./models/credential')
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Credential = require('./models/credential');
+const bcrypt = require('bcrypt'); // Import bcrypt
 
-const templatePath = path.join(__dirname, './views')
+const templatePath = path.join(__dirname, './views');
 
-app.use(express.json())
-app.set("view engine", "hbs")
-app.set("views", templatePath)
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.set('view engine', 'hbs');
+app.set('views', templatePath);
+app.use(express.urlencoded({ extended: false }));
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
 // Connect to the Mongodb database
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Check for connection
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log("Connected to database"))
-
-// const users = [
-//     { name: 'blesson', password: 'password' },
-//     { name: 'kharis', password: 'password' }
-// ]
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database'));
 
 //Login routes
 app.get('/', (req, res) => {
@@ -38,36 +36,49 @@ app.get('/', (req, res) => {
 
 // Signup routes
 app.get('/signup', (req, res) => {
-    res.render("signup")
-})
+  res.render('signup');
+});
 
-app.post('/signup', async (req, res) =>{
+//Password hashing// Setting saltRounds to 10
+app.post('/signup', async (req, res) => {
+  try {
+    const saltRounds = 10; // Number of salt rounds for bcrypt
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds); // Hash the password
+
     const data = {
-        name: req.body.name,
-        password: req.body.password
-    }
-   await Credential.insertMany([data])
-   res.render("home")
-})
+      name: req.body.name,
+      password: hashedPassword, // Store the hashed password in the database
+    };
+    
+    await Credential.insertMany([data]);
+    res.render('home');
+  } catch (error) {
+    res.send('An error occurred while signing up.');
+  }
+});
 
-
+// Login credential
 app.post('/login', async (req, res) => {
-    try {
-        const check = await Credential.findOne({ name: req.body.name })
-        if (check.password === req.body.password) {
-            res.status(201).render("home", { naming: `${req.body.name}` })
-        }
-        else {
-            res.send("incorrect password")
-        }
-        } 
-        catch (e) {
-        res.send("wrong details")
-        }
-})
+  try {
+    const user = await Credential.findOne({ name: req.body.name });
+
+    if (!user) {
+      return res.send('User not found');
+    }
+    const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (passwordMatch) {
+      res.status(201).render('home', { naming: `${req.body.name}` });
+    } else {
+      res.send('Incorrect password');
+    }
+  } catch (error) {
+    res.send('An error occurred while logging in.');
+  }
+});
 
 // Starting server
-const PORT  = 3000
+const PORT = 3000;
 app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-    })
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,84 @@
+let tbody = document.getElementById("tbody");
+let searchInput = document.getElementById("search");
+let locationSelect = document.getElementById("locationSearch");
+
+// Fetch function
+fetch("https://sierrachurches1.onrender.com/churches")
+// fetch("https://churchapi.onrender.com/api/churches")
+    .then(res => res.json())
+    .then(json => {
+        const allData = json;
+        renderTable(allData);
+
+        const uniqueLocations = [...new Set(allData.map(data => data.location))];
+        uniqueLocations.sort(); // Sort locations alphabetically
+
+        uniqueLocations.forEach(location => {
+            const option = document.createElement("option");
+            option.value = location;
+            option.textContent = location;
+            locationSelect.appendChild(option);
+        });
+
+        searchInput.addEventListener("input", filterData);
+        locationSelect.addEventListener("change", filterData);
+
+        function filterData() {
+            const searchTerm = searchInput.value.toLowerCase();
+            const selectedLocation = locationSelect.value.toLowerCase();
+            let filteredData;
+
+            if (selectedLocation === "") {
+                filteredData = allData.filter(data => (
+                    data.name.toLowerCase().includes(searchTerm) ||
+                    data.address.toLowerCase().includes(searchTerm) ||
+                    data.contact.toLowerCase().includes(searchTerm) ||
+                    data.location.toLowerCase().includes(searchTerm) // Add location to input search
+                ));
+            } else {
+                filteredData = allData.filter(data => (
+                    (data.name.toLowerCase().includes(searchTerm) ||
+                    data.address.toLowerCase().includes(searchTerm) ||
+                    data.contact.toLowerCase().includes(searchTerm)) &&
+                    (data.location.toLowerCase() === selectedLocation || selectedLocation === "all")
+                ));
+            }
+
+            renderTable(filteredData);
+        }
+    });
+
+// Render table rows
+function renderTable(data) {
+    tbody.innerHTML = ""; // Clear existing rows
+    data.forEach(dataItem => {
+        tbody.appendChild(td_fun(dataItem));
+    });
+}
+
+// Create td
+function td_fun({ logo, name, address, contact, location }) {
+    let td = document.createElement("tr");
+    td.innerHTML = `
+    <td class="px-6 py-4">
+    <div class="d-flex align-items-center">
+        <div class="flex-shrink-0">
+            <img src="${logo}" class="rounded-circle" alt="" width="50" height="50">
+        </div>
+        <div class="ms-3">
+            <div class="fw-medium text-dark">${name}</div>
+            <div class="text-secondary">${address}</div>
+        </div>
+    </div>
+</td>
+<td class="px-6 py-4">
+    <span class="badge bg-success">${contact}</span>
+</td>
+<td class="px-6 py-4">
+    <span class="text-secondary">${location}</span>
+</td>
+    `;
+    return td;
+}
+
+// document.write('hello');
